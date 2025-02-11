Bug fix of calculation min max

scripts/downloader_scalar.py
@@ -7,18 +7,6 @@ from pathlib import Path
 import numpy as np
 
 
-def get_min_max_local(data):
-    min_values = []
-    max_values = []
-
-    for matrix in data:
-        plane_values = matrix.copy()
-        plane_values = plane_values[~np.isnan(plane_values)]
-        min_values.append(float(np.percentile(plane_values, 2.5)))
-        max_values.append(float(np.percentile(plane_values, 97.5)))
-        return min_values, max_values
-
-
 def download_whole_cube(db, actual_time, variable):
     print(f"Start downloading {variable} data for: ", actual_time)
     start_counter = datetime.now()
@@ -39,7 +27,8 @@ def download_whole_cube(db, actual_time, variable):
     data_array_16 = np.array(data3D_flat, dtype=np.float16)
 
     # extract min and max values for each layer
-    min_values_local, max_values_local = utils.get_min_max_local(data_array_16)
+    min_values_local, max_values_local = utils.get_min_max_local(
+        data_array_16, variable)
 
     # Save each layer separately
     for i, data in enumerate(data_array_16):

scripts/downloader_vort.py
@@ -36,9 +36,10 @@ def calc_all_vorticity():
 
     for filename in os.listdir(path):
         uv_data = os.path.join(path, filename)
-        vort_name = "vorticity_uv" + filename[2:]
+        vort_name = "vorticity_uvw" + filename[2:]
 
-        if os.path.isfile(uv_data) and "uv" in filename:
+        if os.path.isfile(uv_data) and "uv_" in filename:
+            print(filename)
             vorticity_flattend = compute_vorticity_from_file(uv_data)
             np.savetxt(os.path.join(path, vort_name), [vorticity_flattend],
                        delimiter=",")

scripts/utils.py
@@ -47,16 +47,24 @@ def resize_array(data):
     return zoom(data, (zoom_factor, zoom_factor), order=1)
 
 
-def get_min_max_local(data):
+def get_min_max_local(data, variable):
     min_values = []
     max_values = []
 
     for matrix in data:
         plane_values = matrix.copy()
         plane_values = plane_values[~np.isnan(plane_values)]
-        min_values.append(float(np.percentile(plane_values, 2.5)))
-        max_values.append(float(np.percentile(plane_values, 97.5)))
-        return min_values, max_values
+        if variable == "salt":
+            plane_values = plane_values[plane_values >= 30]
+
+        if len(plane_values) != 0:
+            min_values.append(float(np.percentile(plane_values, 5)))
+            max_values.append(float(np.percentile(plane_values, 95)))
+        else:
+            min_values.append(min_values[-1])
+            max_values.append(max_values[-1])
+
+    return min_values, max_values
 
 
 def get_min_max_per_month(dir, prefix):
@@ -69,14 +77,18 @@ def get_min_max_per_month(dir, prefix):
         print("Reading file: ", file_path)
         data = np.loadtxt(file_path, delimiter=",")
         data = data[~np.isnan(data)]
-        min_values.append(float(np.percentile(data, 2.5)))
-        max_values.append(float(np.percentile(data, 97.5)))
+        if len(data) == 0:
+            min_values.append(min_values[-1])
+            max_values.append(max_values[-1])
+        else:
+            min_values.append(float(np.percentile(data, 5)))
+            max_values.append(float(np.percentile(data, 95)))
         counter += 1
     return min_values, max_values
 
 
 def get_min_max_vort():
-    variable = "vorticity_uv"
+    variable = "vorticity_uvw"
     directory = "/data"
 
     min_dict = {}
@@ -93,7 +105,6 @@ def get_min_max_vort():
     month = int(start_time.month)
 
     while actual_time < end_time:
-        print("Processing time: ", actual_time.date())
         prefix = f"{variable}_{year}_{month}_13_0_"
 
         min_values, max_values = get_min_max_per_month(directory, prefix)